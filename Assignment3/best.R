f27b6d3e099c96afc0e6451c59a4106a53d456da
Vineeth Shankar <shankar.vineeth@gmail.com>
2014-11-17 01:50:57 -0500

    Working partially

--- Assignment3/best.R
+++ Assignment3/best.R
@@ -5,8 +5,8 @@
 ## lowest 30-day mortality for the specified outcome
 ## to be added in repo
 best <- function(state,disease) {
-  setwd("/home/vineethshankar/Rdata")
-  outcome <- read.csv("outcome-of-care-measures.csv")
+  #setwd("/home/vineethshankar/Rdata")
+  #outcome <- read.csv("outcome-of-care-measures.csv")
 ## diseases will be in lower case and
 ## states are all upper case in the data frame
   disease <- tolower(disease)
@@ -17,10 +17,10 @@ best <- function(state,disease) {
       stop("invalid outcome")
   }
 ## Split the data on the basis of states  
-  relevant <- outcome[,c(2,11,17,23)]
+  relevant <- outcome[,c(2,7,11,17,23)]
   hosp_by_state <- relevant[relevant$State == state,]
-  cnames <- c("Hospital.Name", "State", "heart attack", "heart failure", "pneumonia")
+  cnames <- c("Hospital.Name","State","heart attack","heart failure","pneumonia")
   colnames(hosp_by_state) <- cnames
-  hosp_by_state <- hosp_by_state[order(hosp_by_state[,disease]),]
+  hosp_by_state <- hosp_by_state[order(hosp_by_state[,disease],hosp_by_state[,1]),]
   as.character(hosp_by_state[[1]][[1]])
 }
