4118cc96628b3d41b927e2e185ec223028127922
Vineeth Shankar <shankar.vineeth@gmail.com>
2014-11-17 20:20:45 -0500

    Works

--- Assignment3/best.R
+++ Assignment3/best.R
@@ -6,7 +6,8 @@
 ## to be added in repo
 best <- function(state,disease) {
   #setwd("/home/vineethshankar/Rdata")
-  #outcome <- read.csv("outcome-of-care-measures.csv")
+## columns will be character vectors  
+  outcome <- read.csv("outcome-of-care-measures.csv", stringsAsFactors = FALSE)
 ## diseases will be in lower case and
 ## states are all upper case in the data frame
   disease <- tolower(disease)
@@ -18,9 +19,17 @@ best <- function(state,disease) {
   }
 ## Split the data on the basis of states  
   relevant <- outcome[,c(2,7,11,17,23)]
+  relevant <- relevant[!(relevant[,3]== "Not Available") & !(relevant[,4]== "Not Available") & !(relevant[,5]== "Not Available"),]
   hosp_by_state <- relevant[relevant$State == state,]
   cnames <- c("Hospital.Name","State","heart attack","heart failure","pneumonia")
   colnames(hosp_by_state) <- cnames
+
+## Converting outcome columns to numeric for proper sorting
+  hosp_by_state[,c(3,4,5)] <- sapply(hosp_by_state[,c(3,4,5)],as.numeric) 
+## final <- transform(hosp_by_state, heart_failure = as.numeric(as.character(heart_failure)))
+
+## ordering by ascending order of mortality rates (1st arg)
+## breaking ties by hospital name (2nd arg)
   hosp_by_state <- hosp_by_state[order(hosp_by_state[,disease],hosp_by_state[,1]),]
   as.character(hosp_by_state[[1]][[1]])
 }
