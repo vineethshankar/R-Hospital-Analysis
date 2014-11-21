ab792322ff3acc17c1b760e3c7dc3360ddbac10d
Vineeth Shankar <shankar.vineeth@gmail.com>
2014-11-20 19:02:56 -0500

    Need to order alphabetically

--- Assignment3/rankall.R
+++ Assignment3/rankall.R
@@ -12,16 +12,14 @@ rankall <- function(disease, num = "best") {
   
   relevant <- outcome[,c(2,7,11,17,23)]
   relevant <- relevant[!(relevant[,3]== "Not Available") & !(relevant[,4]== "Not Available") & !(relevant[,5]== "Not Available"),]
-  cnames_allrank <- c("hospital", "state")
   cnames <- c("Hospital.Name","State","heart attack","heart failure","pneumonia")
   colnames(relevant) <- cnames
   rank <- function(x) {
-    if (num > length(x)) {
-      print(x)
+    if (num > length(x[,2])) {
       return(NA)
     }
       else {
-        x[order(x)][num]
+        x[order(x[,2]),][num,]
     }
   }
 # Method 1: split by state, run a loop through the list, find the hospital of the given rank
@@ -29,13 +27,22 @@ rankall <- function(disease, num = "best") {
 # reduce data by the name of the state, order
   allrank <- data.frame()
 ## List of mortality rates for each state
-  hosp_state <- split(relevant[,disease], relevant$State)
+  hosp_state <- split(relevant[,c("Hospital.Name",disease)], relevant$State) #contains hospital names and 
+## disease rates for each state
 ## NA condition if num > number of hospitals in a state 
 ## Finding the num'th lowest by lapply 
-  final <- lapply(hosp_state, rank)
-  states <- names(final)
-  for(nm in states) {
-    allrank <- rbind(allrank, c(nm, final[[nm]]))
+  final <- lapply(hosp_state, rank) # ordering
+  for(i in 1:length(final)) {
+    if(is.na(final[[i]][1])) {
+      addrow <- data.frame(hospital = NA, state = names(final)[i])
+    }
+      else {
+        addrow <- data.frame(hospital = final[[i]][1,1], state = names(final)[i])
+    }
+    allrank <- rbind(allrank, addrow)
   }
-  all
+  #allrank <- cbind(names(final), unlist(final[1:length(final)],use.names = FALSE))
+  #cnames_allrank <- c("state","hospital")
+  #colnames(allrank) <- cnames_allrank
+  allrank
 }  
